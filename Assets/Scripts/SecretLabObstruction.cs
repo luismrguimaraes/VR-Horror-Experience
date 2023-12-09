c9588fa89bae24b744040d71e34608a79f354238
luismrguimaraes <guimaraesluis0@gmail.com>
2023-12-09 22:11:41 +0000

    feat: create a prefab variant for papers with blood, create secret passage feature

---
+++ Assets/Scripts/SecretLabObstruction.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Composites;
+
+public class SecretLabObstruction : MonoBehaviour
+{
+    public AudioSource draggingSound;
+    public bool isDragging;
+    public bool isOpenning;
+    public float draggingForce = 0.8f;
+    public float dragDuration = 2f;
+    public float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging) {
+            Vector3 currentPos = gameObject.transform.position;
+            if (isOpenning) gameObject.transform.SetPositionAndRotation(new Vector3(currentPos.x, currentPos.y, currentPos.z + draggingForce * Time.deltaTime), gameObject.transform.rotation);
+            else gameObject.transform.SetPositionAndRotation(new Vector3(currentPos.x, currentPos.y, currentPos.z - draggingForce * Time.deltaTime), gameObject.transform.rotation);
+            
+            timer += Time.deltaTime;
+            if (timer > dragDuration){
+                timer = 0;
+                isDragging = false;
+                draggingSound.Stop();
+            }
+        }
+    }
+
+    public void OnLeverActive(){
+        isDragging = true;
+        isOpenning = true;
+        draggingSound.Play();
+    }
+    public void OnLeverDeactive(){
+        isDragging = true;
+        isOpenning = false;
+        draggingSound.Play();
+    }
+
+    void OnDestroy(){
+        draggingSound.Stop();
+    }
+}
