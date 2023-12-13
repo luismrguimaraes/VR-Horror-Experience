d9b5bfc736ad9729852717c7bc0aed9ba09a4c5e
Ricas <ricardo.cool.pinto@gmail.com>
2023-12-13 00:28:51 +0000

    Gameover start

--- Assets/Scripts/GameOverJumpScare.cs
+++ Assets/Scripts/GameOverJumpScare.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class GameOverJumpScare : MonoBehaviour
 {
     public GameObject jumpScareImage;
-    public float jumpScareImageDuration = 0.5f;
+    public float jumpScareImageDuration = 1f;
     public AudioSource jumpScareSound;
     public AudioSource backgroundSound;
 
     float timer;
     bool counting;
 
+
+    void Start()
+    {
+        // find the jumpscare image using find game object with tag
+        jumpScareImage = GameObject.FindGameObjectWithTag("JumpScareImage");
+    }
+
     void OnTriggerEnter(Collider collider){
+
         if (collider.gameObject.layer != LayerMask.NameToLayer("Character Controller")) return;
 
-        jumpScareImage.SetActive(true);
+        jumpScareImage.GetComponent<SpriteRenderer>().enabled = true;
         jumpScareSound.Play();
 
         backgroundSound.Stop();
@@ -27,6 +35,11 @@ public class GameOverJumpScare : MonoBehaviour
     void Update()
     {
         if (counting) timer += Time.deltaTime;
-        if (timer > jumpScareImageDuration) jumpScareImage.SetActive(false);
+        if (timer > jumpScareImageDuration){
+            jumpScareImage.GetComponent<SpriteRenderer>().enabled = false;
+            // Change scene to GameOver scene
+            DontDestroyOnLoad(jumpScareImage);
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+        }
     }
 }
---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+// Quit game script
+
+using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+    public void QuitGameFunction() {
+        Application.Quit();
+    }
+    
+}
